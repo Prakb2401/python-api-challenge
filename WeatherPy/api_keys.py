5c0395f2cb2b1cd5fcb225e4d0545973dab7e62f
Prakb2401 <prakb2401@gmail.com>
2022-08-04 01:57:20 -0400

    weatherpy final

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "d3da663059ec12325bbfc80036049888"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCiCVCmm81daMQKWPmKd6roUnlnxZ2P38w"
