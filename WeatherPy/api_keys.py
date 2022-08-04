66d31e7ae11d7e311693d5052df5a0c964a66d28
Prakb2401 <prakb2401@gmail.com>
2022-08-04 03:53:34 -0400

    accidently deleted all my hw :(

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "513d3defb2026484faa393e28ebe201f"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyCiCVCmm81daMQKWPmKd6roUnlnxZ2P38w"
